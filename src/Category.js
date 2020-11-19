5af98dc2ea7e2ebecda4a089d9fd5dafd6d8a8c4
Ian Campbell <ian.campbell.92@gmail.com>
2020-11-18 20:54:32 -0700

    adding categories page

---
+++ src/Category.js
@@ -0,0 +1,37 @@
+import React from "react";
+import data from "./MOCK_DATA.json";
+
+/**
+ *
+ * @typedef {{title:string,link:string,author:string,level:string,category:string}} book
+ */
+/**
+ *
+ * @param {{book:book}} param0
+ */
+export function Book({ book, key }) {
+  const { title, author, link: download, category } = book;
+  const cutLength = 24;
+  return (
+    <div
+      key={key}
+      className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 mr2 fl"
+    >
+      <div className="h4 f2 fw5 tc">
+        {title.length > cutLength ? title.slice(0, cutLength) + "..." : title}
+      </div>
+      <span className="i f5">{author}</span>
+      <img src={download} alt="" srcset="" className="h4 w4 db" />
+    </div>
+  );
+}
+
+export function Category(props) {
+  const catName = props.match.params.name;
+  return (
+    <div className="ml2 w-75 center">
+      <div className="f1 tc mt3 ttc">{catName}</div>
+      {data.slice(0, 20).map((book, key) => Book({ book, key }))}
+    </div>
+  );
+}
