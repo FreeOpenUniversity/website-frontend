968adb09aafcd6a0aac77cae66271a1c2fd544b9
Ian Campbell <ian.campbell.92@gmail.com>
2020-11-18 20:54:56 -0700

    minor style changes

--- src/CategoriesScroller.js
+++ src/CategoriesScroller.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export function CategoryScroller({ categories }) {
-  console.log(categories);
   return (
     <div>
       {categories.map(({ id, name }) => (
-        <div key={name} className="f1 pa3 avenir bg-light-gray black">
-          <Link to={{ pathname: "/category", state: { id } }}>{name}</Link>
+        <div key={name} className="f1 pa3 avenir bg-light-gray black ttc">
+          <Link
+            to={{ pathname: "/category/" + name.toLowerCase(), state: { id } }}
+          >
+            {name}
+          </Link>
         </div>
       ))}
     </div>
