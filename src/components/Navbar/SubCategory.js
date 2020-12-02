4c61a2859ce4a5459ab1879fe22be4b56c21d924
vrcode <vilasayrathahao@gmail.com>
2020-12-01 22:11:27 -0600

    links

---
+++ src/components/Navbar/SubCategory.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { SubCategoryData } from "./SubCategoryData";
+
+import "./Navbar.css";
+
+export const SubCategory = () => {
+  return (
+    <>
+      <div className="sub_nav_bar">
+        <div className="sub_nav_bar_top ">
+          <h2>Catagories</h2>
+          <button className="fw5 avenir pa3 br-pill b-dark-green bw0 ">
+            View all categories
+          </button>
+        </div>
+
+        {/* <div className="sub_nav_bar_bottom"> */}
+        {SubCategoryData.map((item, index) => {
+          return (
+            <li key={index} className={item.title}>
+              <Link to={item.path}>
+                <div>{item.title}</div>
+              </Link>
+            </li>
+          );
+        })}
+        {/* </div> */}
+      </div>
+    </>
+  );
+};
---
+++ src/components/Navbar/SubCategoryData.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+export const SubCategoryData = [
+  {
+    title: "placeholder1", //category
+    path: "/categories/placeholder1",
+  },
+  {
+    title: "placeholder2",
+    path: "/categories/placeholder2",
+  },
+  {
+    title: "placeholder3",
+    path: "/categories/placeholder3",
+  },
+
+  {
+    title: "placeholder4",
+    path: "/categories/placeholder4",
+  },
+  {
+    title: "placeholder5",
+    path: "/categories/placeholder5",
+  },
+  {
+    title: "placeholder6",
+    path: "/categories/placeholder6",
+  },
+
+  {
+    title: "placeholder7",
+    path: "/categories/placeholder7",
+  },
+  {
+    title: "placeholder8",
+    path: "/categories/placeholder8",
+  },
+  {
+    title: "placeholder9",
+    path: "/categories/placeholder9",
+  },
+  {
+    title: "placeholder10",
+    path: "/categories/placeholder10",
+  },
+  {
+    title: "placeholder11",
+    path: "/categories/placeholder11",
+  },
+
+  {
+    title: "placeholder12",
+    path: "/categories/placeholder12",
+  },
+  {
+    title: "placeholder13",
+    path: "/categories/placeholder13",
+  },
+  {
+    title: "placeholder14",
+    path: "/categories/placeholder14",
+  },
+  {
+    title: "placeholder15",
+    path: "/categories/placeholder15",
+  },
+  {
+    title: "placeholder16",
+    path: "/categories/placeholder16",
+  },
+];
