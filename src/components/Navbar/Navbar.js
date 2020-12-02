21c3ab77736bd0e2e636251834c857f0f1ca2721
vrcode <vilasayrathahao@gmail.com>
2020-12-01 22:10:38 -0600

    links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { SearchBar } from "../../SearchBar";
-import DropdownMenu from "../DropdownMenu/DropdownMenu";
+import { Link } from "react-router-dom";
 
 import "./Navbar.css";
+
+import logo from "./freeopenu88.png";
+import { SubCategory } from "./SubCategory";
 
 export const Navbar = () => {
   return (
@@ -11,59 +14,31 @@ export const Navbar = () => {
         {/* <div> */}
         <ul>
           <div className="nav_bar_content_main avenir">
-            <p>Logo</p>
-            <li className="dropdown">
-              Books
-              {/* <button>View all categories</button> */}
-              <ul className="sub_nav_bar">
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
-                <li>placeholder</li>
+            <Link to="#">
+              <img src={logo} height="50px" />
+            </Link>
 
-                {/* <Category name={bookname}/>  */}
-                {/* <Category name="Philosophy"/>
-                                <Category name="Philosophy2"/>
-                                <Category name="Philosophy3"/>
-                                <Category name="Philosophy4"/>
-                                <Category name="Philosophy5"/>
-                                <Category name="Philosophy6"/>
-                                <Category name="Philosophy7"/>
-                                <Category name="Philosophy8"/>
-                                <Category name="Philosophy9"/>
-                                <Category name="Philosophy10"/>
-                                <Category name="Philosophy11"/>
-                                <Category name="Philosophy12"/> */}
-              </ul>
-            </li>
-            <li>Program and Degree</li>
-            <li>Partners</li>
-            <li>Others</li>
+            <Link
+              to="#"
+              className="link white hover-green hover-animate dropdown"
+            >
+              <p>Categories</p>
+              <SubCategory />
+            </Link>
+            <Link to="#" className="link white hover-green hover-animate">
+              <p>Subject</p>
+            </Link>
+            <Link to="#" className="link white hover-green hover-animate">
+              <p>For Study</p>
+            </Link>
+            <Link to="#" className="link white hover-green hover-animate">
+              <p>Others</p>
+            </Link>
           </div>
+
           <div className="nav_bar_content_side">
             <SearchBar />
-            {/* <DropdownMenu/> */}
-            <button className="button">Register</button>{" "}
+            <button className="button">Register</button>
             {/*convert into a button component */}
           </div>
         </ul>
