7cbcd57fcbb86867fc19035fc5a420189772e227
Ian Campbell <ian.campbell.92@gmail.com>
2020-12-03 10:03:19 -0700

    navbar, footer wraps routes in app

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { SearchBar } from "./SearchBar";
 import { CategoryScroller } from "./CategoriesScroller";
-import Footer from "./components/Footer";
 import data from "./MOCK_DATA.json";
 import _ from "lodash";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import DropdownMenu from "./components/DropdownMenu/DropdownMenu";
-import { AccountSetting } from "./components/AccountSetting/AccountSetting";
-import { MyLearning } from "./components/MyLearning/MyLearning";
-import { Navbar } from "./components/Navbar/Navbar";
 
 export const FrontPage = () => {
   return (
     <div className="avenir">
-      <header>
-        <DropdownMenu />
-      </header>
-
-      <SearchBar />
-      <Navbar />
       <CategoryScroller
         categories={_.uniq(
           data
@@ -27,7 +14,6 @@ export const FrontPage = () => {
         ).map((name) => ({ name }))}
       />
       <br />
-      <Footer />
     </div>
   );
 };
