2d32dbf062790cb7719bf1ef8871185b483d9b2b
gaby13524 <54719206+gaby13524@users.noreply.github.com>
2020-12-18 10:12:05 -0800

    Update classPage.js
    Updated the class page to show the 3 categories and added header

--- src/components/classPage/classPage.js
+++ src/components/classPage/classPage.js
@@ -1,26 +1,42 @@
 import React from "react";
 import ClassList from "./ClassList";
-import Footer from "../Footer";
-
-let dumData = {
-  title: "Class 1: Intro to Something",
-  desc:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque augue quis ligula dignissim porttitor. Quisque eu enim efficitur, tincidunt eros eu, bibendum nunc. Donec ornare mauris a lorem dignissim, vitae rhoncus sem lobortis. Cras in leo dapibus, finibus turpis eu, pellentesque turpis. Nunc sit amet mauris semper, pharetra erat a, fringilla urna. Suspendisse consectetur, ligula a venenatis varius, lacus massa semper arcu, sed dictum mauris eros venenatis eros.",
-  imgLink:
-    "https://insektenliebe.com/wp-content/uploads/2018/05/Cubaris-spec.-Rubber-Ducky-9-bearbeitet.jpg",
-  imgAlt: "Rubber ducky",
-  progress: "32",
-  links: {
-    chapters: "#",
-    scores: "#",
-    keepG: "#",
-  },
-};
+import Footer from "../Footer/index";
+import Header from "../Header/Header"; 
+import dumData from "./classDumData.json";
 
 function App() {
+  let inProg = [],
+    compl = [],
+    favorts = [];
+  dumData.forEach((data) => {
+    if (data.progress == 0.0) {
+      favorts.push(data);
+    } else if (data.progress < 1.0) {
+      inProg.push(data);
+    } else {
+      compl.push(data);
+    }
+  });
+
   return (
     <div>
-      <ClassList {...dumData} />
+      <Header />
+      <h1 className="mid-gray athelas tracked-tight pl4 f3"> In Progress </h1>
+      {inProg.map((data) => (
+        <ClassList {...data} />
+      ))}
+
+      <hr />
+      <h1 className="mid-gray athelas tracked-tight pl4 f3"> Completed </h1>
+      {compl.map((data) => (
+        <ClassList {...data} />
+      ))}
+
+      <hr />
+      <h1 className="mid-gray athelas tracked-tight pl4 f3"> Favorites </h1>
+      {favorts.map((data) => (
+        <ClassList {...data} />
+      ))}
       <Footer />
     </div>
   );
