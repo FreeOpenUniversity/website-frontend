2a47d1fbd71b093e8683e3b0fc3427e00369b1cf
URpineapple <zhang.te@usky.neu.edu>
2020-12-21 22:12:21 -0500

    refactor App.js based on redux

--- src/App.js
+++ src/App.js
@@ -12,63 +12,51 @@ import ContactUs from "./components/ContactUs";
 import FAQ from "./components/FAQ";
 import Header from "./components/Header/Header";
 import { api } from "./store";
-import { Component } from "react";
 import classPage from "./components/classPage/classPage";
 import ScrollToTop from "./components/ScrollToTop";
+import { connect } from "react-redux";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: {},
-      categories: {},
-    };
-  }
-
-  componentDidMount() {
-    //get book data from API
-    api.book.read().then((result) => this.setState({ books: result.payload }));
-    api.category.read().then((result) => console.log("result", result));
-  }
-  render() {
-    const { books } = this.state;
-    // console.log(l.then(result => console.log('result', result.payload)));
-    const routes = [
-      { path: "/category/:name", as: Category },
-      {
-        path: "/book/:id",
-        as: (props) => {
-          const id = props.match.params.id;
-          return <Book data={books[id] ? books[id] : MOCK_DATA[id - 1]}></Book>;
-        },
+function App(props) {
+  const { books } = props;
+  const routes = [
+    { path: "/category/:name", as: Category },
+    {
+      path: "/book/:id",
+      as: (props) => {
+        const id = props.match.params.id;
+        return <Book data={books[id] ? books[id] : MOCK_DATA[id - 1]}></Book>;
       },
-      { path: "/", as: <FrontPage /> },
-      { path: "/about-us", as: AboutUs },
-      { path: "/contact-us", as: ContactUs },
-      { path: "/my-classes", as: classPage },
-      { path: "/setting", as: AccountSetting },
-      { path: "/profile", as: Profile },
-      { path: "/questions", as: FAQ },
-    ];
+    },
+    { path: "/", as: <FrontPage /> },
+    { path: "/about-us", as: AboutUs },
+    { path: "/contact-us", as: ContactUs },
+    { path: "/my-classes", as: classPage },
+    { path: "/setting", as: AccountSetting },
+    { path: "/profile", as: Profile },
+    { path: "/questions", as: FAQ },
+  ];
 
-    return (
-      <>
-        <Header />
-        <ScrollToTop>
-          <Switch>
-            {routes.map(({ path, as }) => {
-              return (
-                <Route key={path} path={path} exact>
-                  {as}
-                </Route>
-              );
-            })}
-          </Switch>
-        </ScrollToTop>
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <ScrollToTop>
+        <Switch>
+          {routes.map(({ path, as, index }) => {
+            return (
+              <Route key={index} path={path} exact>
+                {as}
+              </Route>
+            );
+          })}
+        </Switch>
+      </ScrollToTop>
+      <Footer />
+    </>
+  );
 }
 
-export default App;
+function mapStateToProps(state, props) {
+  return { categories: state.category, books: state.book, ...props };
+}
+
+export default connect(mapStateToProps)(App);
