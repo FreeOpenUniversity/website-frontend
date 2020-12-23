38c1c7199ecd9c1cf39320adfba454db9b15e4d6
vrcode <vilasayrathahao@gmail.com>
2020-12-23 05:05:42 -0600

    removes dependency, reorganize breadcrumbs logic

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -8,41 +8,43 @@ const Breadcrumbs = (props) => {
   } = props;
   const pathnames = pathname.split("/").filter((x) => x);
   return (
-    <div className="avenir pa2 ph5 link flex">
-      {pathnames.length > 0 && (
-        <div>
+    <div>
+      {pathnames.length >
+        0 /*if pathnames then return the whole crumbs, else nothing*/ && (
+        <div className="avenir pa2 ph5 link flex">
+          {/* homecrumbs */}
           <Link
-            className="no-underline avenirlink hover-green hover-animate"
+            className="no-underline avenir link hover-green hover-animate"
             onClick={() => history.push("/")}
           >
             <i className="fas fa-home link black"></i> {/*home logo*/}
           </Link>
-          <span className="ph2"> > </span>{" "}
-          {/* breadcrumb separator after home*/}
+          <span className="ph2"> > </span> {/*separator following home*/}
+          {/* the rest of the crumbs */}
+          {pathnames.map((name, index) => {
+            const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+            const isLast = index === pathnames.length - 1;
+
+            return isLast ? (
+              // make the end the crumbs text , not link
+              <div className="ttc" key={name}>
+                {name}
+              </div>
+            ) : (
+              <div className="flex">
+                <Link
+                  key={name}
+                  className="no-underline  fw4 avenirlink hover-green hover-animate flex"
+                  onClick={() => history.push(routeTo)}
+                >
+                  <div className="ttc"> {name} </div>
+                </Link>
+                <span className="ph2"> > </span> {/* breadcrumb separator*/}
+              </div>
+            );
+          })}
         </div>
       )}
-
-      {pathnames.map((name, index) => {
-        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
-        const isLast = index === pathnames.length - 1;
-
-        return isLast ? (
-          <div className="ttc" key={name}>
-            {name}
-          </div>
-        ) : (
-          <div className="flex">
-            <Link
-              key={name}
-              className="no-underline  fw4 avenirlink hover-green hover-animate flex"
-              onClick={() => history.push(routeTo)}
-            >
-              <div className="ttc"> {name} </div>
-            </Link>
-            <span className="ph2"> > </span> {/* breadcrumb separator*/}
-          </div>
-        );
-      })}
     </div>
   );
 };
